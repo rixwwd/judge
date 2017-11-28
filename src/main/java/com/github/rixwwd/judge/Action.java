de703481faa3a7b12732a7e624fcef77171ebdb6
rixwwd <rixwwd@users.noreply.github.com>
2017-11-29 01:35:50 +0900

    ActionのtoStringを追加。

--- src/main/java/com/github/rixwwd/judge/Action.java
+++ src/main/java/com/github/rixwwd/judge/Action.java
@@ -2,10 +2,40 @@ package com.github.rixwwd.judge;
 
 public enum Action {
 
-	REJECT,
-	CHALLENGE,
-	AUTHNETICATED,
-	CANCEL,
-	UNAVAILABLE,
-	ATTEMPT
+	REJECT {
+		@Override
+		public String toString() {
+			return "reject";
+		}
+	},
+	CHALLENGE {
+		@Override
+		public String toString() {
+			return "challenge";
+		}
+	},
+	AUTHNETICATED {
+		@Override
+		public String toString() {
+			return "authenticated";
+		}
+	},
+	CANCEL {
+		@Override
+		public String toString() {
+			return "cancel";
+		}
+	},
+	UNAVAILABLE {
+		@Override
+		public String toString() {
+			return "unavailable";
+		}
+	},
+	ATTEMPT {
+		@Override
+		public String toString() {
+			return "attempt";
+		}
+	}
 }
